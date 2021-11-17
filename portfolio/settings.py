3e40d4a5a0b573caa7f381c3b27c30a73049b479
fabrown40 <fabrown40@gmail.com>
2021-11-17 19:37:15 +0100

    fuck encore merde

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,14 +126,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
 
